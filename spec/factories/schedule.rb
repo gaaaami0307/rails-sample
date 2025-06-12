6ca8f8357eab449caa28e1cba43788c3c7e5c20d
Gami <s1320016@u-aizu.ac.jp>
2025-06-12 22:52:52 +0900

    test

--- spec/factories/schedule.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :schedule do
-    sequence(:movie_id) { |n| n }
-    sequence(:start_time) { Time.now }
-    sequence(:end_time) { |n| Time.now + n.minutes }
-  end
-end
--- spec/factories/user.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    name { "test" }
-    sequence(:email) { |n| "TEST#{n}@example.com" }
-    password { "testuser" }
-    password_confirmation { "testuser" }
-  end
-end
